9007c2da9586e1d549bab27b35cced8b65435216
Hossein Fallahi <fallahi.raziuni@gmail.com>
2021-06-10 07:55:13 +0430

    Update tcga.R

--- tcga.R
+++ tcga.R
@@ -1,4 +1,4 @@
-# Using TCGA biolinks bioconductor package to extract a local version of the data into your computer
+# Using [TCGA biolinks](https://bioconductor.org/packages/release/bioc/html/TCGAbiolinks.html) in the bioconductor package to extract a local version of the data into your computer
 library("TCGAbiolinks") 
 query_TCGA = GDCquery(
   project = "TCGA-SKCM",
